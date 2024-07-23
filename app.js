a3882d4de07d69fdfdbdf3a53921732492defe04
VIKRANT-1903 <168111813+VIKRANT-1903@users.noreply.github.com>
2024-07-23 16:33:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+    console.log("New WebSocket connection");
+
+    socket.on("send-location", (data) => {
+        console.log("Location received:", data); // Log received location data
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", function (){
+        io.emit("user disconnected", socket.id);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
